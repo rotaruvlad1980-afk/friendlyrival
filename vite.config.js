4f5c8bf531364ac2e1b3f28fba50d3c21d6134d3
rotaruvlad1980-afk <rotaruvlad1980@gmail.com>
2026-05-25 13:45:26 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'UNRESOLVED_IMPORT') throw new Error(warning.message)
+        warn(warning)
+      }
+    }
+  }
 })
